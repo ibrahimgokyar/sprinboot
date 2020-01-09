f36f6f5aa835ffdeca32eff220171fbda8b19820
ibrahimgokyar <ibrahim.gokyar1@gmail.com>
2020-01-08 20:05:20 -0600

    spring boot proje

---
+++ src/test/java/com/altay/altayspringboot/AltaySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altay.altayspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltaySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
